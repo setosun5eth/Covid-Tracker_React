ea495cde09a1eb2e33d5f1dabdf28d20dc818a8c
unknown <setosun5.eth>
2024-11-26 10:16:50 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import countriesJson from "./countries.json";
+import TopPage from "./pages/TopPage";
+import "./App.css";
 
 function App() {
+  const [country, setCountry] = useState();
+  const [countryData, setCountryData] = useState({
+    date: "",
+    newConfirmed: "",
+    totalConfirmed: "",
+    newRecovered: "",
+    totalRecoverd: "",
+  });
+
+  const getCountryData = () => {
+    fetch(
+      `https://monotein-books.vercel.app/api/corona-tracker/country/${country}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setCountryData({
+            date: data[data.length -1].Date,
+            newConfirmed: data[data.length -1].Confirmed - data[data.length -2].Confirmed,
+            totalConfirmed: data[data.length -1].Confirmed,
+            newRecovered:  data[data.length -1].Recovered- data[data.length -2].Recovered,
+            totalRecovered: data[data.length -1].Recovered,
+        });
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TopPage
+        countriesJson={countriesJson}
+        setCountry={setCountry}
+        getCountryData={getCountryData}
+        countryData={countryData }
+      />
     </div>
   );
 }
