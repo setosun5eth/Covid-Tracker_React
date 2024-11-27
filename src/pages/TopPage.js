c5c6323881209b7774032ab002b34bc6c6abd8a4
setosun5.eth <110733054+setosun5eth@users.noreply.github.com>
2024-11-27 14:55:35 +0900

    Update TopPage.js

--- src/pages/TopPage.js
+++ src/pages/TopPage.js
@@ -1,17 +1,18 @@
-import Title from "../components/Title";
-import Selector from "../components/Selector";
-import Results from "../components/Results";
+import React from 'react';
+import Title from '../components/Title';
+import Selector from '../components/Selector';
+import Results from '../components/Results';
 
 const TopPage = (props) => {
-    return (
-        <div className="top-page-container">
-            <div>
-                <Title />
-                <Selector countriesJson={props.countriesJson} setCountry={props.setCountry} getCountryData={props.getCountryData} />
-                <Results countryData={props.countryData} />
-            </div>
-        </div>
-    );
-};
+  return (
+    <div className="top-page-container">
+      <div>
+      <Title />
+      <Selector countriesJson={props.countriesJson} setCountry={props.setCountry} getCountryData={props.getCountryData} />
+      <Results countryData={props.countryData} />
+      </div>
+    </div>
+  )
+}
 
-export default TopPage;
+export default TopPage
